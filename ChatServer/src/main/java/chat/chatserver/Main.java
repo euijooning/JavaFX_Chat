e75ec6692bae81d61b7d658018666561fb66ed28
EUIJOON <euijoonism@gmail.com>
2023-11-21 15:36:26 +0900

    서버 화면 구현

--- ChatServer/src/main/java/chat/chatserver/Main.java
+++ ChatServer/src/main/java/chat/chatserver/Main.java
@@ -1,6 +1,14 @@
 package chat.chatserver;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -84,7 +92,66 @@ public class Main extends Application {
     // UI를 생성하고, 실질적으로 프로그램을 동작시키는 메서드
     @Override
     public void start(Stage stage) throws IOException {
+        // 레이아웃을 하나 만들어서 요소를 담기
+        BorderPane pane = new BorderPane();
+        pane.setPadding(new Insets(5));;
 
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false); // 수정 불가
+        textArea.setFont(new Font("맑은 고딕", 15));
+        pane.setCenter(textArea);
+
+        // 시작 스위치 만들기
+        Button toggleButton = new Button("시작하기");
+        toggleButton.setMaxWidth(Double.MAX_VALUE);
+        BorderPane.setMargin(toggleButton, new Insets(1, 0, 0, 0));
+        pane.setBottom(toggleButton); // 아래 위치에 담기
+
+        String IP = "127.0.0.1"; // 내 로컬(루프백) 주소, 일단 내 컴퓨터 안에서
+        int port = 9876;
+
+        // 이벤트 발생(버튼 눌렀을 때) 처리
+        toggleButton.setOnAction(event -> {
+            if (toggleButton.getText().equals("시작하기")) {
+                startServer(IP, port);
+                // 버튼을 눌렀을 때 함수를 이용하여 처리하는 형태이다.
+                Platform.runLater(() -> {
+                    String message = String.format("[서버 시작]\n", IP, port);
+                    textArea.appendText(message);
+                    toggleButton.setText("종료하기");
+
+                    // 추가: 서버 시작 메시지 팝업
+                    showAlert("서버 시작", "서버가 시작되었습니다.");
+                });
+            } else {
+                // 종료하기 버튼 누르면
+                stopServer(); // 버튼 종료되고
+                Platform.runLater(() -> {
+                    String message = String.format("[서버 종료]\n", IP, port);
+                    textArea.appendText(message);
+                    toggleButton.setText("시작하기");
+
+                    // 추가: 서버 종료 메시지 팝업
+                    showAlert("서버 종료", "서버가 종료됩니다.");
+                });
+            }
+        });
+
+        Scene scene = new Scene(pane, 500, 500);
+        stage.setTitle("[ 채팅 서버 ]");
+        stage.setOnCloseRequest(event -> stopServer());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    // 추가: Alert 팝업 메시지 표시 메서드
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     // 시작점
